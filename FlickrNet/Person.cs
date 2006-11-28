a6272b69c65a802f4466d7b9786fbfd73a1f626c
Sam Judson <samjudson@gmail.com>
2006-11-28 09:13:39 +0000

    Added Views, MailBoxSha1Hash, MobileUrl and IconFarm properties. Fixed FirstTakenDate field as well.
    git-svn-id: https://flickrnet.svn.codeplex.com/svn@12232 81e6e737-5a20-4d0f-a97c-cb01297b46e0

--- FlickrNet/Person.cs
+++ FlickrNet/Person.cs
@@ -12,58 +12,87 @@ namespace FlickrNet
 	[System.Serializable]
 	public class Person
 	{
+		private string _userId;
+		private int _isAdmin;
+		private int _isPro;
+		private int _iconServer;
+		private int _iconFarm;
+		private string _username;
+		private string _realname;
+		private string _location;
+		private PersonPhotosSummary _summary = new PersonPhotosSummary();
+		private string _photosUrl;
+		private string _profileUrl;
+		private string _mobileUrl;
+		private string _mboxHash;
+
 		/// <summary>The user id of the user.</summary>
 		/// <remarks/>
 		[XmlAttribute("nsid", Form=XmlSchemaForm.Unqualified)]
-		public string UserId;
+		public string UserId { get { return _userId; } set { _userId = value; } }
     
 		/// <summary>Is the user an administrator. 
 		/// 1 = admin, 0 = normal user.</summary>
 		/// <remarks></remarks>
 		[XmlAttribute("isadmin", Form=XmlSchemaForm.Unqualified)]
-		public int IsAdmin;
+		public int IsAdmin { get { return _isAdmin; } set { _isAdmin = value; } }
 
 		/// <summary>Does the user posses a pro account.
 		/// 0 = free acouunt, 1 = pro account holder.</summary>
 		[XmlAttribute("ispro", Form=XmlSchemaForm.Unqualified)]
-		public int IsPro;
+		public int IsPro { get { return _isPro; } set { _isPro = value; } }
 	
 		/// <summary>Does the user posses a pro account.
 		/// 0 = free acouunt, 1 = pro account holder.</summary>
 		[XmlAttribute("iconserver", Form=XmlSchemaForm.Unqualified)]
-		public int IconServer;
+		public int IconServer { get { return _iconServer; } set { _iconServer = value; } }
+	
+		/// <summary>No idea what purpose this field serves.</summary>
+		[XmlAttribute("iconfarm", Form=XmlSchemaForm.Unqualified)]
+		public int IconFarm { get { return _iconFarm; } set { _iconFarm = value; } }
 	
 		/// <summary>The users username, also known as their screenname.</summary>
 		[XmlElement("username", Form=XmlSchemaForm.Unqualified)]
-		public string UserName;
+		public string UserName { get { return _username; } set { _username = value; } }
 	
 		/// <summary>The users real name, as entered in their profile.</summary>
 		[XmlElement("realname", Form=XmlSchemaForm.Unqualified)]
-		public string RealName;
+		public string RealName { get { return _realname; } set { _realname = value; } }
+	
+		/// <summary>The SHA1 hash of the users email address - used for FOAF networking.</summary>
+		[XmlElement("mbox_sha1sum", Form=XmlSchemaForm.Unqualified)]
+		public string MailBoxSha1Hash { get { return _mboxHash; } set { _mboxHash = value; } }
 	
 		/// <summary>Consists of your current location followed by country.</summary>
 		/// <example>e.g. Newcastle, UK.</example>
 		[XmlElement("location", Form=XmlSchemaForm.Unqualified)]
-		public string Location;
+		public string Location { get { return _location; } set { _location = value; } }
 
 		/// <summary>Sub element containing a summary of the users photo information.</summary>
 		/// <remarks/>
 		[XmlElement("photos", Form=XmlSchemaForm.Unqualified)]
-		public PersonPhotosSummary PhotosSummary;
+		public PersonPhotosSummary PhotosSummary { get { return _summary; } set { _summary = value; } }
 
 		/// <summary>
 		/// The users photo location on Flickr
 		/// http://www.flickr.com/photos/username/
 		/// </summary>
 		[XmlElement("photosurl",Form=XmlSchemaForm.Unqualified)]
-		public string PhotosUrl;
+		public string PhotosUrl { get { return _photosUrl; } set { _photosUrl = value; } }
 
 		/// <summary>
 		/// The users profile location on Flickr
 		/// http://www.flickr.com/people/username/
 		/// </summary>
 		[XmlElement("profileurl",Form=XmlSchemaForm.Unqualified)]
-		public string ProfileUrl;
+		public string ProfileUrl { get { return _profileUrl; } set { _profileUrl = value; } }
+
+		/// <summary>
+		/// The users mobile home page on Flickr
+		/// http://www.flickr.com/mod/photostream.gne?id=xxx/
+		/// </summary>
+		[XmlElement("mobileurl",Form=XmlSchemaForm.Unqualified)]
+		public string MobileUrl { get { return _mobileUrl; } set { _mobileUrl = value; } }
 
 		/// <summary>
 		/// Returns the <see cref="Uri"/> for the users Buddy Icon.
@@ -87,6 +116,9 @@ namespace FlickrNet
 	[System.Serializable]
 	public class PersonPhotosSummary
 	{
+		private int _photoCount;
+		private int _views;
+
 		/// <summary>The first date the user uploaded a picture, converted into <see cref="DateTime"/> format.</summary>
 		[XmlIgnore()]
 		public DateTime FirstDate
@@ -98,19 +130,36 @@ namespace FlickrNet
 		[XmlIgnore()]
 		public DateTime FirstTakenDate
 		{
-			get { return Utils.UnixTimestampToDate(firsttakendate_raw); }
+			get 
+			{
+				if( firsttakendate_raw == null || firsttakendate_raw.Length == 0 ) return DateTime.MinValue;
+				return System.DateTime.Parse(firsttakendate_raw);
+			}
 		}
 
 		/// <summary>The total number of photos for the user.</summary>
 		/// <remarks/>
 		[XmlElement("count", Form=XmlSchemaForm.Unqualified)]
-		public int PhotoCount;
+		public int PhotoCount
+		{
+			get { return _photoCount; }
+			set { _photoCount = value; }
+		}
+
+		/// <summary>The total number of photos for the user.</summary>
+		/// <remarks/>
+		[XmlElement("views", Form=XmlSchemaForm.Unqualified)]
+		public int Views
+		{
+			get { return _views; }
+			set { _views = value; }
+		}
 
 		/// <remarks>The unix timestamp of the date the first photo was uploaded.</remarks>
 		[XmlElement("firstdate", Form=XmlSchemaForm.Unqualified)]
 		public string firstdate_raw;
 
-		/// <remarks>The unix timestamp of the date the first photo was uploaded.</remarks>
+		/// <remarks>The date the first photo was taken.</remarks>
 		[XmlElement("firsttakendate", Form=XmlSchemaForm.Unqualified)]
 		public string firsttakendate_raw;
 
