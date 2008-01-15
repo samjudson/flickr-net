755d67bf95a79bc5f074c77320074625621c8984
Sam Judson <samjudson@gmail.com>
2008-01-15 16:04:48 +0000

    Updated Place information needed by Flickr.PhotosGetInfo.
    git-svn-id: https://flickrnet.svn.codeplex.com/svn@29631 81e6e737-5a20-4d0f-a97c-cb01297b46e0

--- FlickrNet/PhotoLocation.cs
+++ FlickrNet/PhotoLocation.cs
@@ -9,6 +9,15 @@ namespace FlickrNet
 	/// </summary>
 	public class PhotoLocation
 	{
+		private double _latitude;
+		private double _longitude;
+		private GeoAccuracy _accuracy;
+		private string _placeId;
+		private PlaceInformation _locality;
+		private PlaceInformation _county;
+		private PlaceInformation _region;
+		private PlaceInformation _country;
+
 		/// <summary>
 		/// Default constructor
 		/// </summary>
@@ -20,18 +29,82 @@ namespace FlickrNet
 		/// The latitude of the photo.
 		/// </summary>
 		[XmlAttribute("latitude", Form=XmlSchemaForm.Unqualified)]
-		public double Latitude;
+		public double Latitude
+		{
+			get { return _latitude; } set { _latitude = value; }
+		}
 
 		/// <summary>
 		/// The longitude of the photo.
 		/// </summary>
 		[XmlAttribute("longitude", Form=XmlSchemaForm.Unqualified)]
-		public double Longitude;
+		public double Longitude
+		{
+			get { return _longitude; } set { _longitude = value; }
+		}
 
 		/// <summary>
 		/// The accuracy of the location information. See <see cref="GeoAccuracy"/> for accuracy levels.
 		/// </summary>
 		[XmlAttribute("accuracy", Form=XmlSchemaForm.Unqualified)]
-		public GeoAccuracy Accuracy;
+		public GeoAccuracy Accuracy		
+		{
+			get { return _accuracy; } set { _accuracy = value; }
+		}
+
+		/// <summary>
+		/// The Place ID of the geolocation of this photo.
+		/// </summary>
+		[XmlAttribute("place_id")]
+		public string PlaceId
+		{
+			get { return _placeId; } set { _placeId = value; }
+		}
+
+		/// <summary>
+		/// The locality for the geolocation for this photo.
+		/// </summary>
+		[XmlElementAttribute("locality")]
+		public PlaceInformation Locality
+		{
+			get { return _locality; } set { _locality = value; }
+		}
+
+		[XmlElementAttribute("county")]
+		public PlaceInformation County
+		{
+			get { return _county; } set { _county = value; }
+		}
+
+		[XmlElementAttribute("region")]
+		public PlaceInformation Region
+		{
+			get { return _region; } set { _region = value; }
+		}
+
+		[XmlElementAttribute("country")]
+		public PlaceInformation Country
+		{
+			get { return _country; } set { _country = value; }
+		}
+
+	}
+
+	public class PlaceInformation
+	{
+		private string _placeId;
+		private string _placeName;
+
+		[XmlAttribute("place_id")]
+		public string PlaceId
+		{
+			get { return _placeId; } set { _placeId = value; }
+		}
+
+		[XmlText()]
+		public string PlaceName
+		{
+			get { return _placeName; } set { _placeName = value; }
+		}
 	}
 }
