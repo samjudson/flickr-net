a70ffaad5f7bf597c4b0c89be8c33b8fc72ffbdd
Sam Judson <samjudson@gmail.com>
2012-08-13 15:22:11 +0000

    Huge update. Added numerous new methods, as well as a ton of new unit tests.
    git-svn-id: https://flickrnet.svn.codeplex.com/svn@99161 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ FlickrNet/GroupInfoCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlickrNet
+{
+    /// <summary>
+    /// List containing <see cref="GroupInfo"/> items.
+    /// </summary>
+    public sealed class GroupInfoCollection : System.Collections.ObjectModel.Collection<GroupInfo>, IFlickrParsable
+    {
+        void IFlickrParsable.Load(System.Xml.XmlReader reader)
+        {
+            if (reader.LocalName != "groups")
+                UtilityMethods.CheckParsingException(reader);
+
+            reader.Read();
+
+            while (reader.LocalName == "group")
+            {
+                GroupInfo member = new GroupInfo();
+                ((IFlickrParsable)member).Load(reader);
+                Add(member);
+            }
+
+            reader.Skip();
+        }
+    }
+}
